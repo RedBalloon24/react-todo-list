5834de21c2c61340df956fa236410af670890e91
Anonymous <worldtraveler757@gmail.com>
2020-08-18 16:50:33 +0200

    Updates

---
+++ precache-manifest.c45f3b934b3658c72576669a1bfac133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cfdaabfac5da3d7f1228ec7fec49cc",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "5de29ddcb3671c6f2429",
+    "url": "/react-todo-list/static/css/main.f4fde535.chunk.css"
+  },
+  {
+    "revision": "144be3f8dd09e831b37f",
+    "url": "/react-todo-list/static/js/2.7553992d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.7553992d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de29ddcb3671c6f2429",
+    "url": "/react-todo-list/static/js/main.8f61f4b1.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
